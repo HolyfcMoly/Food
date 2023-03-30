9468817e5cb50f34dce25db5479bf261d023ab39
Vlad <sambipush@mail.ru>
2023-03-30 14:27:22 +0300

    добавлен скрипт для открытия модального окна после того как пользователь долистает до конца стр

--- js/main.js
+++ js/main.js
@@ -164,4 +164,13 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   const modalTimerId = setTimeout(openModal, 5000)
+
+  function showModalByScroll() {
+    if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+      openModal()
+      window.removeEventListener('scroll', showModalByScroll)
+    }
+  }
+
+  window.addEventListener('scroll', showModalByScroll)
 });
